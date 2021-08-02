7e68386a7fd008b0e42d41f685a39786719edc66
Yamy <yamilagorrin@gmail.com>
2021-08-02 17:48:20 -0400

    Proyecto Time_display

---
+++ time_display/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+from django.shortcuts import render
+from time import gmtime, strftime, localtime
+    
+def index(request):
+    context = {
+        "time": strftime("%b %d, %Y", localtime()),
+        "hour": strftime("%H:%M %p", localtime()),
+    }
+    return render(request,'index.html', context)
+
+
+def second(request, name):
+    return HttpResponse('Hola ' + name)
+
